10f892846b954f7539b238b2209e2d575cedbd33
marwan agmy <marwanagmy53@gmail.com>
2026-09-08 17:25:46 +0300

    Add OnBoardingView and update routing for onboarding screen

---
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingPageView extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(children: [
+        
+      ],
+    );
+  }
+}
